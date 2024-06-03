edde27fab2267b49936944c1ad18f60fad15828d
Alex-Rabaev <rabaevav@live.com>
2024-06-03 23:01:40 +0300

    add: created view creating a recipe and for getting all recipe

--- recipe_book/recipes/views.py
+++ recipe_book/recipes/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+
+class RecipeCreateOrListAll(APIView):
+    def post(self, request):
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+        if not recipes:
+            return Response({"message": "There are no recipes yet"}, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
