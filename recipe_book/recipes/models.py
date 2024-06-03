28a033430132c4d686e49e242235c2281a3811a6
Alex-Rabaev <rabaevav@live.com>
2024-06-03 21:20:22 +0300

    add: added model Recipe, updated settings.py

--- recipe_book/recipes/models.py
+++ recipe_book/recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    ingredients = models.JSONField()
+    instructions = models.TextField()
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
